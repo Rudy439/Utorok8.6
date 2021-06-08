38711f0e8941c15e796ae72b3d6980863ddd1cbf
Rudy439 <rudolf.mato@kosickaakademia.sk>
2021-06-08 11:58:57 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final WeatherStation weatherStation = new WeatherStation();
+
+        final CurrentConditions currentConditions = new CurrentConditions();
+        weatherStation.registerObserver(currentConditions);
+        final ForecastDisplay forecastDisplay = new ForecastDisplay();
+        weatherStation.registerObserver(forecastDisplay);
+        final StatisticsDisplay statisticsDisplay = new StatisticsDisplay();
+        weatherStation.registerObserver(statisticsDisplay);
+
+        final Random random = new Random();
+        for (int i = 0; i < 5; i++) {
+            weatherStation.setMeasurements(random.nextFloat(), random.nextFloat(), random.nextFloat());
+            System.out.println("***************************************");
+        }
+    }
+}
---
+++ src/StatisticsDisplay.java
@@ -0,0 +1,6 @@
+public class StatisticsDisplay implements Observer {
+
+    public void update(float temp, float humidity, float pressure) {
+        System.out.println(String.format("%f\t%f\t%f", temp, humidity, pressure));
+    }
+}
